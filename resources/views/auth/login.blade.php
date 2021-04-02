1056619411ac465573a1e2ed599e3d197c6088a8
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2021-04-02 16:53:02 +0500

    qeswfergte45gtty

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,29 @@
 @extends('auth.master')
 
 @section('content')
-                <form method="POST" action="{{ route('login') }}">
+{{-- <main class=""> --}}
+  <form class=" col-lg-6 form-signin">
+    <img class="mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 text-muted">© 2017–2021</p>
+  </form>
+    {{-- <form method="POST" action="{{ route('login') }}">
                     @csrf
 					<div class="form-group">
 						<label for="exampleInputEmail1">Логин
@@ -26,7 +48,7 @@
 					<br>
 					<button type="submit" class="btn btn-primary">Войти</button>
 				</form>
-				<br>
+				<br> --}}
 				{{-- <button class="btn btn-primary">Добавить</button> --}}
 
 @endsection
--- resources/views/auth/master.blade.php
+++ resources/views/auth/master.blade.php
@@ -9,24 +9,13 @@
 </head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<style>
-	html, body {
-		height: 100%;
-	}
-	.container {
-		height: 100%;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-	}
-</style>
+
 <body>
-	<div class="container">
-		<div class="row">
+	<div class="text-center">
 			{{-- <div class="col-lg-4"></div> --}}
-			<div class="col-lg-6">
+			{{-- <div class="col-lg-6"> --}}
 				@yield('content')
-			</div>
+			{{-- </div> --}}
 			{{-- <div class="col-lg-4"></div> --}}
 		</div>
 	</div>

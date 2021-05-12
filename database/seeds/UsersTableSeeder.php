908c8d5c028d8665861faa5d2fbb74bf511ab370
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2021-05-12 13:52:04 +0500

    seed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Администратор',
+            'username' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('12345678'),
+        ]);
+    }
+    }
+}
